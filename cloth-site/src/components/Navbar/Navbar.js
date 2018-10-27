2d04e26b4ccf638f9c9949615a4817921a37da86
DeboraArzu <deboranise29@hotmail.com>
2018-10-27 00:39:53 -0600

    Navbar

---
+++ cloth-site/src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Navbar.css';
+import '../SideDrawer/DrawerToggleButton';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+
+
+const navbar = props => (
+    <header className="navbar">
+        <nav className="navbar-navigation">
+            <div>
+                <DrawerToggleButton/>
+            </div>
+            <div className="navbar-logo">
+                <a href="/">LOGO</a> 
+            </div>
+            <div className="space"/>
+            <div className="navbar-navigation-items">
+                <ul>
+                    <li>
+                        <a href="/">Products</a>
+                    </li>
+                    <li>
+                        <a href="/">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default navbar;
