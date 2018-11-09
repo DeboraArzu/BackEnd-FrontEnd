0bfcb61ded5b6b875a8724a827d7e09a0fe2e708
DeboraArzu <deboranise29@hotmail.com>
2018-11-09 16:36:02 -0600

    redis

--- ProductApp/routes/product.route.js
+++ ProductApp/routes/product.route.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-// Require the controllers WHICH WE DID NOT CREATE YET!!
 const product_controller = require('../controllers/product.controller');
 
-
-// a simple test url to check that all of our files are communicating correctly.
+// get all products
 router.get('/products', product_controller.getproducts);
 
 //HTTP POST
--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const product = require('./ProductApp/routes/product.route'); // Imports routes for the products
 const app = express();
+//redis cache
+const path = require('path');
+const axios = require('axios');
+const redis = require('redis');
+// connect to Redis
+const REDIS_URL = process.env.REDIS_URL;
+const client = redis.createClient(REDIS_URL);
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
@@ -21,6 +28,13 @@ let port = 4000;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
+});
+
+client.on('connect', () => {
+    console.log(`connected to redis`);
+});
+client.on('error', err => {
+    console.log(`Error: ${err}`);
 });
 
 module.exports = app;
